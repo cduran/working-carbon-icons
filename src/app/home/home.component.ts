13be0cfaa97f320e43ef7f02d49a395128d0d8f8
Carlos Duran <cduran@ibm.com>
2021-08-12 09:01:45 -0600

    Diferent ways to import icons FYI: template dashes usage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,13 @@ import { IconService } from 'carbon-components-angular';
 import AddIcon16 from '@carbon/icons/es/add/16';
 import UserIcon16 from '@carbon/icons/es/user/16';
 import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
-import Bee20 from '@carbon/icons/es/bee/20'
+import Bee20 from '@carbon/icons/es/bee/20';
+
+import { Purchase32 } from '@carbon/icons/es';
+import { Accessibility24 } from '@carbon/icons/es';
+import { AirlineDigitalGate20 } from '@carbon/icons/es';
+import { AirportLocation16 } from '@carbon/icons/es';
+import { ApplicationWeb32 } from '@carbon/icons/es';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +22,16 @@ export class HomeComponent implements OnInit {
   constructor(private iconService: IconService) {}
 
   ngOnInit(): void {
-    this.iconService.registerAll([AddIcon16, UserIcon16, UserAvatar20, Bee20]);
+    this.iconService.registerAll([
+      AddIcon16,
+      UserIcon16,
+      UserAvatar20,
+      Bee20,
+      Purchase32,
+      Accessibility24,
+      AirlineDigitalGate20,
+      AirportLocation16,
+      ApplicationWeb32,
+    ]);
   }
 }
