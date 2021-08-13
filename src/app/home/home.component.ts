1521b7d27dcf583a81e1af5e67bf193773f7369e
Carlos Duran <cduran@ibm.com>
2021-08-13 08:33:17 -0600

    Enabling cache in builds Reverting to use direct icon imports

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,12 +6,11 @@ import AddIcon16 from '@carbon/icons/es/add/16';
 import UserIcon16 from '@carbon/icons/es/user/16';
 import UserAvatar20 from '@carbon/icons/es/user--avatar/20';
 import Bee20 from '@carbon/icons/es/bee/20';
-
-import { Purchase32 } from '@carbon/icons/es';
-import { Accessibility24 } from '@carbon/icons/es';
-import { AirlineDigitalGate20 } from '@carbon/icons/es';
-import { AirportLocation16 } from '@carbon/icons/es';
-import { ApplicationWeb32 } from '@carbon/icons/es';
+import Purchase32 from '@carbon/icons/es/purchase/32';
+import Accessibility24 from '@carbon/icons/es/accessibility/24';
+import AirlineDigitalGate20 from '@carbon/icons/es/airline--digital-gate/20';
+import AirportLocation16 from '@carbon/icons/es/airport-location/16';
+import ApplicationWeb32 from '@carbon/icons/es/application--web/32';
 
 @Component({
   selector: 'app-home',
