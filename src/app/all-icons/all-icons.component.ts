62e68fa089a25c96bb74c3b68e06b4edfbb216ed
Carlos Duran <cduran@ibm.com>
2021-08-20 09:50:44 -0600

    Moving all icons register to for loop

--- src/app/all-icons/all-icons.component.ts
+++ src/app/all-icons/all-icons.component.ts
@@ -8,27 +8,24 @@ import * as Icons from '@carbon/icons/es';
 @Component({
   selector: 'app-all-icons',
   templateUrl: './all-icons.component.html',
-  styleUrls: ['./all-icons.component.scss']
+  styleUrls: ['./all-icons.component.scss'],
 })
-
 export class AllIconsComponent implements OnInit {
   groupedIcons = [];
 
   constructor(private iconService: IconService) {}
 
-	ngOnInit() {
-		const iconMap = new Map();
-		for (const [key, descriptor] of Object.entries(Icons)) {
-			if (!iconMap.has(descriptor["name"])) {
-				iconMap.set(descriptor["name"], []);
-			}
-			iconMap.get(descriptor["name"]).push(descriptor);
-		}
-		this.groupedIcons = Array.from(iconMap.values());
+  ngOnInit() {
+    const iconMap = new Map();
+    for (const [key, descriptor] of Object.entries(Icons)) {
+      // Registering all icons
+      this.iconService.register(descriptor as object);
 
-    // Registering all icons
-    Object.entries(Icons).map((icon) => {
-      this.iconService.register(icon[1] as object);
-    });
+      if (!iconMap.has(descriptor['name'])) {
+        iconMap.set(descriptor['name'], []);
+      }
+      iconMap.get(descriptor['name']).push(descriptor);
+    }
+    this.groupedIcons = Array.from(iconMap.values());
   }
 }
